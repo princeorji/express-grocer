57e0747685f503a9f27f34b9444b8525c428d641
princeorji <princemikeorji@gmail.com>
2024-10-02 08:42:30 -0700

    refactor

--- src/controller/product.js
+++ src/controller/product.js
@@ -1,21 +1,6 @@
 const Product = require('../models/product');
 const mongoose = require('mongoose');
 
-const create = async (req, res, next) => {
-  const { name, description, imageUrl, price } = req.body;
-
-  try {
-    if (!name || !description || !imageUrl || !price) {
-      return res.status(400).json({ error: 'Parameters missing' });
-    }
-
-    const products = await Product.create(req.body);
-    res.status(201).json(products);
-  } catch (error) {
-    next(error);
-  }
-};
-
 const products = async (req, res, next) => {
   try {
     const products = await Product.find();
@@ -44,55 +29,7 @@ const getById = async (req, res, next) => {
   }
 };
 
-const update = async (req, res, next) => {
-  const { id } = req.params;
-  const { name, description, imageUrl, price } = req.body;
-
-  try {
-    if (!mongoose.isValidObjectId(id)) {
-      return res.status(400).json({ error: 'Invalid product id' });
-    }
-
-    if (!name || !description || !imageUrl || !price) {
-      return res.status(400).json({ error: 'Parameters missing' });
-    }
-
-    const product = await Product.findByIdAndUpdate(id, req.body, {
-      new: true,
-    });
-    if (!product) {
-      return res.status(404).json({ error: 'Product not found' });
-    }
-
-    res.status(200).json(product);
-  } catch (error) {
-    next(error);
-  }
-};
-
-const remove = async (req, res, next) => {
-  const { id } = req.params;
-
-  try {
-    if (!mongoose.isValidObjectId(id)) {
-      return res.status(400).json({ error: 'Invalid product id' });
-    }
-
-    const product = await Product.findByIdAndDelete(id);
-    if (!product) {
-      return res.status(404).json({ error: 'Product not found' });
-    }
-
-    res.sendStatus(200);
-  } catch (error) {
-    next(error);
-  }
-};
-
 module.exports = {
-  create,
   products,
   getById,
-  update,
-  remove,
 };
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,14 +1,9 @@
 const express = require('express');
 const controller = require('../controller/product');
-const passport = require('passport')
 
 const routes = express.Router();
 
 routes.get('', controller.products);
 routes.get('/:id', controller.getById);
-
-routes.post('', passport.authenticate('jwt', { session: false }),controller.create);
-routes.patch('/:id', passport.authenticate('jwt', { session: false }),controller.update);
-routes.delete('/:id', passport.authenticate('jwt', { session: false }),controller.remove);
 
 module.exports = routes;
